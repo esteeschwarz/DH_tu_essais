dc40dbb9d432ceca66592d3c147a634389704b1c
estee <91368979+esteeschwarz@users.noreply.github.com>
2022-05-23 15:40:35 +0200

    tut

--- sessions/tut5.R
+++ sessions/tut5.R
@@ -11,3 +11,26 @@ lessing3<-read_xml(src5)
 write_xml(lessing1,"data/lessing_emilia.xml")
 write_xml(lessing2,"data/lessing_nathan.xml")
 write_xml(lessing3,"data/lessing_misogyn.xml")
+
+files<-list.files(pattern="*lessing*",path = "sessions/data",include.dirs = T,full.names = T)
+data_1 <- map(files, read_xml)
+library(purr)
+data_1 <- map(files, read_xml)
+# Namespace entfernen
+
+data_1 %>% xml_find_all('//*') %>%
+data%>% xml_path()
+data_1
+# Beispiel 1: xpath-Pfade anzeigen
+# Alle xpath-Pfade anzeigen
+data %>% 
+  xml_find_all('//*') %>%
+  xml_path()
+?xml_path
+
+# Alle xpath-Pfade zu einem head-Element anzeigen
+all_heads <- data %>% 
+  xml_find_all('//category') %>%
+  xml_path()
+all_heads
+all_heads[1]

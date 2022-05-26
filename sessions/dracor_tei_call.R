89529d3824c2f45314f01316588850e88e17543a
estee <91368979+esteeschwarz@users.noreply.github.com>
2022-05-26 07:26:16 +0200

    init. wks.

---
+++ sessions/dracor_tei_call.R
@@ -0,0 +1,32 @@
+#12215.dramenanalyse
+#20220525(16.43)
+####################
+# API fetch dracor TEI
+if (!require("jsonlite","httr","purrr","xml2")) 
+  install.packages("jsonlite","httr","purrr","xml2")
+library(jsonlite)
+library(httr)
+library(purrr)
+library(xml2)
+corpusname<-"ger"
+base_url<-"https://dracor.org/api"
+full_url<-paste(base_url,"corpora",corpusname,sep="/")
+api_call<-httr::GET(full_url)
+httr::content(api_call,"text")
+api_df<-jsonlite::fromJSON(content(api_call,"text"),simplifyDataFrame = T)
+plays<-api_df$dramas
+#################
+#12216.ascension
+#sample GET: 
+get<-"https://dracor.org/api/corpora/ger/play/buechner-woyzeck/tei"
+
+#extract play GETs:
+#all buechner plays:
+seek<-"wagner"
+grep1<-grep(seek,plays$name)
+grep1
+get.url<-plays$name[grep1]
+tei_url_full<-paste(play_url,map_chr(get.url,paste),"tei",sep="/")
+corpus3<-map(tei_url_full,read_xml)
+#wks.
+#####

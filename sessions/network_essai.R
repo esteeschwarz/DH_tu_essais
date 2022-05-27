f28f1cf9965ad831a427b8ca66e8ea4d672cd6a4
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-05-27 22:42:36 +0200

    no

---
+++ sessions/network_essai.R
@@ -0,0 +1,11 @@
+#12217.network essai
+#20220527(18.54)
+####################
+getwd()
+getwd()
+library(xml2)
+library(purrr)
+dta<-read_xml("~/boxHKW/21S/DH/gith/DH_essais/data/net/faust_klingemann.gexf")
+
+
+         
